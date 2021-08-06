c5c3d9feb71b24bd6ba1c120a32f3848078fdd3f
Ejiro Oke <ejiroogheneoke@gmail.com>
2021-08-06 09:50:09 +0100

    git remote add origin https://github.com/Ejiro-Oke/React-Movie-Search-App.git

---
+++ src/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Test(){
+    
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+//to create the SearchMovies component
+//form with a class of form
+//label with htmlFor="query" and a class of Label
+//input of type text with a name of "query" and a placeholder
+//button class of button and a type of submit
